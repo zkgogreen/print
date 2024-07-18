2ed9356f585b9c9f2a5b7e92d60077954fd6826a
zakaria <jakaa354.ja@gmail.com>
2024-07-18 16:21:13 +0700

    250

--- print/views.py
+++ print/views.py
@@ -20,49 +20,37 @@ def generate_and_print_pdf(request):
 
     # Define the file path
     pdf_path = 'receipt.pdf'
-    y = 1400
+    y = 1600
     # Create a PDF file
-    c = canvas.Canvas(pdf_path, pagesize=(227, y))  # 227 points = 80mm width
-    c.setFont("Helvetica", 12)
+    c = canvas.Canvas(pdf_path, pagesize=(250, y))  # 250 points = 80mm width
+    c.setFont("Helvetica",13)
     
     # Function to draw justified text
     def draw_justified_text(c, text_left, text_right, y):
-        c.drawString(12, y, text_left)
-        text_width = c.stringWidth(text_right, "Helvetica", 12)
-        c.drawString(227 - 12 - text_width, y, text_right)
+        c.drawString(10, y, text_left)
+        text_width = c.stringWidth(text_right, "Helvetica",13)
+        c.drawString(250 - 10 - text_width, y, text_right)
     
     
     y -= 20
     
     # Add content to the PDF
-    c.drawString(12, y, toko)
+    c.drawString(10, y, toko)
     y -= 20
-    c.drawString(12, y, datetime_str)
+    c.drawString(10, y, datetime_str)
     y -= 20
-    c.drawString(12, y, number)
+    c.drawString(10, y, number)
     y -= 20
-    c.drawString(12, y, "-"*60)
+    c.drawString(10, y, "-"*60)
     y -= 20
     draw_justified_text(c, "Total Pembayaran:", f"Rp.{pembayaran}", y)
     y -= 20
-    c.drawString(12, y, "-"*60)
+    c.drawString(10, y, "-"*60)
     y -= 20
     draw_justified_text(c, f"{metode_bayar}:", f"Rp.{total}", y)
     y -= 20
     draw_justified_text(c, "Kembalian:", f"Rp.{kembalian}", y)
-    # draw_justified_text(c, "Receipt No:", number, y)
-    # y -= 20
-    # draw_justified_text(c, "waktu:", datetime_str, y)
-    # y -= 20
-    # c.drawString(10, y, "-"*30)
-    # y -= 20
-    # draw_justified_text(c, "Total Pembayaran:", f"Rp.{pembayaran}", y)
-    # y -= 20
-    # draw_justified_text(c, "Total Bayar:", f"Rp.{total}", y)
-    # y -= 20
-    # draw_justified_text(c, "Kembalian:", f"Rp.{kembalian}", y)
-    
-    # Finish up and save the PDF
+
     c.showPage()
     c.save()
     
