19d0784d9dba4309abe500b265de2bd93932b0f4
Muhammad Zakaria <jakaa354.ja@gmail.com>
2024-08-01 22:33:04 +0700

    no space

--- print/views.py
+++ print/views.py
@@ -100,13 +100,13 @@ def generate_and_print_pdf(request):
         draw_justified_text(c, f"  {arr[13]}x {arr[5]} {arr[4]}", f"Rp.{arr[14] or arr[15]}", y)
         y -= 15
         for item in arr[6:12]:
-            if item == '':
+            if item == '': 
                 continue
-            c.drawString(data.padding + 27, y, f"{item}")
+            c.drawString(data.padding, y, f"{item}")
             # draw_justified_text(c, " ", f"{item}", y)
             # draw_justified_text(c, f"  {item}", y)
             y -= 15
-        c.drawString(data.padding + 27, y, f"{arr[17]}")
+        c.drawString(data.padding, y, f"{arr[17]}")
         y -= 15
 
 
